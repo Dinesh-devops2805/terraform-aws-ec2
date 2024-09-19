f6114ac79f51ecd0b4c26b035b81e39dee9bad60
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-19 15:26:56 +0530

    module

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
 
-    ami = "ami-09c813fb71547fc4f"  # this AMI ID may change over the time 
-    instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0525227f8e227493c"]  # we used allow-all security group ID here 
+    ami = var.ami_id  # this AMI ID may change over the time 
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_ids  # we used allow-all security group ID here 
 }
